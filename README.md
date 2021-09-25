20f87e26070bd7dd01b8595db6a555e6e69b06b6
alexzanderr <alexxander18360@gmail.com>
2021-09-26 01:52:57 +0300

    upgraded readme

--- README.md
+++ README.md
@@ -34,3 +34,80 @@ in `init.lua`:
 require("nvim-treesitter-statusline")
 ```
 
+
+## why?
+
+vs code always had this status line where you can see your cursor location in the current file
+
+i really wanted this in `neovim` and also other people can try this and be glad.
+
+
+## source code
+```lua
+local Popup = require("nui.popup")
+local event = require("nui.utils.autocmd").event
+local Split = require("nui.split")
+
+function NvimTreeSitterStatusline()
+    -- mount/open the component
+    local treesitter_statusline = Popup({
+        enter = false,
+        focusable = false,
+        relative = "editor",
+        border = {},
+        position = {
+            row = 38,
+            col = 0,
+        },
+        -- zindex = 50,
+        size = {
+            width = 100,
+            height = 1,
+        },
+        buf_options = {
+            modifiable = true,
+            readonly = false,
+        },
+    })
+    treesitter_statusline:mount()
+    local lsp_status = require("nvim-treesitter").statusline({
+        indicator_size = 100,
+        separator = " ❱ ",
+        transform_fn = function(line)
+            return line:gsub("%s*[%[%(%{]*%s*$", "")
+        end
+,
+        type_patterns = {
+            "class",
+            "function",
+            "method",
+            "import",
+            "for",
+            "if",
+            "while",
+            "variable",
+            "comment",
+        },
+    })
+    vim.api.nvim_buf_set_lines(treesitter_statusline.bufnr, 0, 1, false, {
+        lsp_status,
+    })
+end
+
+
+-- without this you are garbage
+vim.g.scrolloff = 1
+
+vim.cmd("autocmd CursorHold * lua NvimTreeSitterStatusline()")
+vim.cmd("autocmd CursorHoldI * lua NvimTreeSitterStatusline()")
+vim.cmd("autocmd BufWrite * lua NvimTreeSitterStatusline()")
+vim.cmd("autocmd BufEnter * lua NvimTreeSitterStatusline()")
+
+```
+
+this is the `init.lua` from the plugin
+
+if you can make this better, please submit `PR`s
+
+
+peace!
--- lua/nvim-treesitter-statusline/init.lua
+++ lua/nvim-treesitter-statusline/init.lua
@@ -4,11 +4,9 @@ local Popup = require("nui.popup")
 local event = require("nui.utils.autocmd").event
 local Split = require("nui.split")
 
--- https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup
-
-function FloatingWindow()
+function NvimTreeSitterStatusline()
     -- mount/open the component
-    local floating_window = Popup({
+    local treesitter_statusline = Popup({
         enter = false,
         focusable = false,
         relative = "editor",
@@ -27,7 +25,7 @@ function FloatingWindow()
             readonly = false,
         },
     })
-    floating_window:mount()
+    treesitter_statusline:mount()
     local lsp_status = require("nvim-treesitter").statusline({
         indicator_size = 100,
         separator = " ❱ ",
@@ -35,8 +33,6 @@ function FloatingWindow()
             return line:gsub("%s*[%[%(%{]*%s*$", "")
         end
 ,
-        -- type_patterns = {'module', 'class', 'function', 'method', 'variable', 'comment', 'number', 'import'}
-        -- module is wrong, it prints the file comment in the script
         type_patterns = {
             "class",
             "function",
@@ -49,8 +45,7 @@ function FloatingWindow()
             "comment",
         },
     })
-    -- print(lsp_status)
-    vim.api.nvim_buf_set_lines(floating_window.bufnr, 0, 1, false, {
+    vim.api.nvim_buf_set_lines(treesitter_statusline.bufnr, 0, 1, false, {
         lsp_status,
     })
 end
@@ -59,8 +54,7 @@ end
 -- without this you are garbage
 vim.g.scrolloff = 1
 
--- vim.cmd("autocmd InsertLeave * lua FloatingWindow()")
-vim.cmd("autocmd CursorHold * lua FloatingWindow()")
-vim.cmd("autocmd CursorHoldI * lua FloatingWindow()")
-vim.cmd("autocmd BufWrite * lua FloatingWindow()")
-vim.cmd("autocmd BufEnter * lua FloatingWindow()")
+vim.cmd("autocmd CursorHold * lua NvimTreeSitterStatusline()")
+vim.cmd("autocmd CursorHoldI * lua NvimTreeSitterStatusline()")
+vim.cmd("autocmd BufWrite * lua NvimTreeSitterStatusline()")
+vim.cmd("autocmd BufEnter * lua NvimTreeSitterStatusline()")
